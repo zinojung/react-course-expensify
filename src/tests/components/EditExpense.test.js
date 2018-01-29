9b6ab71a4248e854e974276f39a17f648bacf872
ZINO\46fir <46firstmate@gmail.com>
2018-01-29 23:49:30 +0900

    Setup production build and server

---
+++ src/tests/components/EditExpense.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { EditExpense } from '../../components/EditExpense';
+import expenses from '../fixtures/expenses';
+
+test('should render EditExpense', () => {
+    
+});
